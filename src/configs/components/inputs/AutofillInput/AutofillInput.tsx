8f8aad748432a3087633ae6cca5ecd73beb9921f
Qba Walat <kubawalat@gmail.com>
2023-03-20 21:29:06 +0100

    feat: :construction: autofill input component skeleton

--- src/configs/components/inputs/AutofillInput/AutofillInput.tsx
+++ src/configs/components/inputs/AutofillInput/AutofillInput.tsx
@@ -1,7 +1,33 @@
-import React, { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 
 const AutofillInput: FunctionComponent = () => {
-  return <p>stasis wall</p>;
+  const [fieldValue, setFieldValue] = useState('');
+  return (
+    <>
+      <input
+        value={fieldValue}
+        onChange={({ target: { value } }) => {
+          setFieldValue(value);
+        }}
+        aria-label={'autofill input'}
+        className={'bg-amber-500'}
+      />
+      {INPUT_OPTIONS.filter((option) => option.toLowerCase().includes(fieldValue.toLowerCase())).map((option) => (
+        <p
+          role={'presentation'}
+          onClick={() => {
+            setFieldValue(option);
+          }}
+          key={option}
+          className={'text-cyan-600 text-5xl cursor-pointer hover:text-green-400'}
+        >
+          {option}
+        </p>
+      ))}
+    </>
+  );
 };
+
+const INPUT_OPTIONS = ['stasis', 'wall', 'goat'];
 
 export default AutofillInput;
