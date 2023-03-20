db50fdee45be3765e00f4857db82a4650a5321f6
Kuba Walat <35037208+qbawalat@users.noreply.github.com>
2023-03-20 16:36:46 +0100

    refactor: :rocket: censorship

--- src/configs/components/inputs/AutofillInput/AutofillInput.tsx
+++ src/configs/components/inputs/AutofillInput/AutofillInput.tsx
@@ -1,7 +1,7 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent } from "react";
 
 const AutofillInput: FunctionComponent = () => {
-  return <p>chuj</p>;
+  return <p>lasagne</p>;
 };
 
 export default AutofillInput;
