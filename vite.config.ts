b88e87fe8b304eec9b3e3f3477c358598a50abae
Qba Walat <kubawalat@gmail.com>
2023-04-19 20:17:05 +0200

    fix: :bug: add basepath to config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  base: '/stasis-wall-pkd-frontend',
   test: {
     globals: true,
     environment: 'jsdom',
