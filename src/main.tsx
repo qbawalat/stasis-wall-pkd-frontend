49aceb4bcbb7757937421bf7822e90e7b7d10b3b
Qba Walat <kubawalat@gmail.com>
2023-06-23 19:23:58 +0200

    feat: :sparkles: use custom font
    Keep in mind the font is under private usage only license

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { StrictMode } from 'react';
 import './index.css';
+import './fonts/MADE Outer Sans Alt Light PERSONAL USE.otf';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
