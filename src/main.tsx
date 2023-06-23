25b74615baa612f31ff21c0166db90663986adb3
Qba Walat <kubawalat@gmail.com>
2023-06-23 19:46:58 +0200

    feat: :sparkles: open source font
    Replace custom font files with open source free for commercial use font

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { StrictMode } from 'react';
 import './index.css';
-import './fonts/MADE Outer Sans Alt Light PERSONAL USE.otf';
+import './fonts/SpaceGrotesk-Bold.ttf';
+import './fonts/SpaceGrotesk-Light.ttf';
+import './fonts/SpaceGrotesk-Medium.ttf';
+import './fonts/SpaceGrotesk-Regular.ttf';
+import './fonts/SpaceGrotesk-SemiBold.ttf';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
